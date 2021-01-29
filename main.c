b5d8f0866cf7feb13b620dfed8a4e50739639074
CameronP99 <cammypollock20@gmail.com>
2021-01-29 21:06:47 +0000

    Updated main file

--- main.c
+++ main.c
@@ -4,7 +4,18 @@
 int main(void) {
     // while true get next line (string)
     // get user name to display before command
-    print_display_prompt();
+
+
+    while(1) {
+        print_display_prompt();
+
+        char input[512];
+        gets(input);
+
+        if(!strcmp(input, "exit")) {
+            exit(1);
+        }
+    }
     // pass line to parser.c to return a list of commands (strings)
     // send list of commands to command.c to execute
     return 0;
