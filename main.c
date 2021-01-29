d4bfb352d03c6c119ba58876c64e41781ebd4920
Alessandro <alessandro.lekkas@startmail.com>
2021-01-29 19:48:13 +0100

    Add skeleton

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+
+int main(void) {
+    // get user name to display before command
+    struct passwd *passwd_entry = getpwuid(getuid());
+    if (passwd_entry == NULL) {
+        printf("Could not get user name.");
+        exit(1);
+    }
+    printf("Name: %s $ ", passwd_entry->pw_name);
+    printf("\n");
+    // while true get next line (string)
+    // pass line to parser.c to return a list of commands (strings)
+    // send list of commands to command.c to execute
+    return 0;
+}
