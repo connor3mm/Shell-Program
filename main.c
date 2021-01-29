af75aab6bf60ff4284119c62e1860a0fe86f6963
Alessandro <alessandro.lekkas@startmail.com>
2021-01-29 20:49:17 +0100

    Add last folder to prompt, move function

--- main.c
+++ main.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <pwd.h>
+#include "display.h"
 
 int main(void) {
     // while true get next line (string)
     // get user name to display before command
-    struct passwd *passwd_entry = getpwuid(getuid());
-    if (passwd_entry == NULL) {
-        printf("Could not get user name.");
-        exit(1);
-    }
-    printf("Name: %s $ ", passwd_entry->pw_name);
+    print_display_prompt();
     // pass line to parser.c to return a list of commands (strings)
     // send list of commands to command.c to execute
     return 0;
