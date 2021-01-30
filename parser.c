fcf233da5bf3b91373f03a0df419851a10b2508b
Alessandro <alessandro.lekkas@startmail.com>
2021-01-30 19:00:34 +0100

    Process forking logic

--- parser.c
+++ parser.c
@@ -3,26 +3,42 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <pwd.h>
 
 /*
- * This function will allow the user to input intoa command to the terminal
- */
-void input_command(char* input) {
-
+* This function will allow the user to input into a command to the terminal
+*/
+void input_command(char *input) {
 
     // using delimiters to slip the input
 
-    char *pChr = strtok (input, " \t|><&;");
+    char *pChr = strtok(input, "\t|><&;");
+
     char *tokens[50];
     int index = 0;
 
     while (pChr != NULL) {
 
         tokens[index] = pChr;
-        printf ("%s ", tokens[index]);
+        printf("%s ", tokens[index]);
         index++;
-        pChr = strtok (NULL, " \t|><&;");
+
+        int pid = fork();
+        if (pid == -1) {
+            exit(1);
+        } else if (pid == 0) {
+            // todo: use strtok_r to split command line arguments
+            char *arg_list = {tokens[index], NULL};
+            execv(tokens[index], &arg_list);
+            printf("The current process is (%u)\n", getpid());
+            exit(1);
+        } else {
+            printf("The current non-0 process is (%u)\n", getpid());
+            int state;
+            // todo: redirect stderr and stdout
+            waitpid(pid, &state, 0);
+            pChr = strtok(NULL, "\t|><&;");
+        }
     }
-    putchar ('\n');
 }
