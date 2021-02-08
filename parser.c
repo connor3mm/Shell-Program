e1bdb3c566f2856f0078a8a492442a34dfcb2a27
Alessandro <alessandro.lekkas@startmail.com>
2021-02-08 17:32:57 +0100

    Use execvpe, pass PATH

--- parser.c
+++ parser.c
@@ -1,9 +1,13 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <errno.h>
+
+#define _GNU_SOURCE
 
 /*
 * This function will allow the user to input into a command to the terminal
@@ -31,11 +35,21 @@ void input_command(char *input) {
 
     int pid = fork();
     if (pid < 0) {
-        printf("fork() failed");
-        exit(1);
+        printf("fork() failed\n");
+        return;
+
     } else if (pid == 0) { // child process
-        char *env = {NULL}; //todo: pass PATH
-        execve(tokens[0], tokens, &env);
+        char* path_value = getenv("PATH");
+        // copy the path into PATH=value format with the path_key_value buffer
+        int keyLength = strlen("PATH=");
+        int valueLength = strlen(path_value);
+        char* path_key_value = malloc(keyLength + valueLength + 1);
+        strncpy(path_key_value, "PATH=", keyLength);
+        strncpy(path_key_value + keyLength, path_value, valueLength);
+
+//        printf("%s\n", path_key_value);
+        char *env = {path_key_value, NULL};
+        execvpe(tokens[0], tokens, &env);
         // exec functions do not return if successful, this code is reached only due to errors
         printf("Error: %s\n", strerror(errno));
         exit(1);
