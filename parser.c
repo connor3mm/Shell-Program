1eda31801331d30dfc89b6484452df194188ae93
Alessandro <alessandro.lekkas@startmail.com>
2021-02-09 13:53:22 +0100

    Remove debug print

--- parser.c
+++ parser.c
@@ -26,7 +26,6 @@ void input_command(char *input) {
             break;
         }
         tokens[index] = pChr;
-        printf("%s ", tokens[index]);
         pChr = strtok(NULL, " \t|><&;");
         index++;
     }
