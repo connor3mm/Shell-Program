f6c5ea423d2601e7adef90357fb6f5d0ed607292
Ramsey Ediku CS2019 <ramsey.ediku.2020@uni.strath.ac.uk>
2021-02-11 16:34:36 +0000

    Added setpath and getpath functionalities

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(void) {
     // this should be 0 on successful run, 1 on error
     int statusCode = 0;
 
-    char *currentPath = getenv("PATH"); //Gets current path so we can set it on exit
+    char *currentPath = getenv("PATH"); // Gets current path so we can set it on exit
 
     //Get the home directory
     char *homeDirectory = getenv("HOME");
@@ -56,10 +56,16 @@ int main(void) {
         if (!strcmp(tokens[0], "exit")) {
             break;
         } else if (!strcmp(tokens[0], "getpath")) {
-            // get the PATH
-            continue;
+            printf("%s\n", getenv("PATH")); 
         } else if (!strcmp(tokens[0], "setpath")) {
-            // set the PATH
+            if (tokens[1] == NULL)
+            {
+                printf("Error, setpath requires an argument.\n");
+            } else
+            {
+                setenv("PATH", tokens[1], 1);
+            }
+            
             continue;
         } else {
             int pid = fork();
